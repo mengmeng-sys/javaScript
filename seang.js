d4b875a4cae28742adda066b2d9475f59d923d2a
mengmeng-sys <na634997@gmail.com>
2025-08-05 13:55:01 +0700

    new

---
+++ seang.js
@@ -0,0 +1,28 @@
+console.log("hello wellcome to meng seang page");
+// for(let i= 0 ; i<1000 ; i++){
+// console.log("Sorry beb", i+1);
+// }
+
+// let love=0;
+// while(love<12){
+//  console.log(love);
+//  love++;
+// }
+
+// the greeting card 
+// const name="meng";
+// const greeting="hello,";
+// console.log(greeting,name);
+
+// Calculate aga
+// const age=2006;
+// let current=2025;
+// let result=current-age;
+// console.log(result);
+
+// my favourite foods
+
+// let foods=["kuy teav","amok","cha knhei"];
+// for(let i = 0 ; i<foods.length ; i++){
+//  console.log("I want to eat",foods[i]);
+// }
